5660089c0f8108b7a5460a11235c119c8c6799e9
Totem <77220546+Xv3nm@users.noreply.github.com>
2023-03-21 06:15:54 +0000

    Format js and tabs

--- script.js
+++ script.js
@@ -1,73 +1,90 @@
-$(function () {
-    $(".menu-link").click(function () {
-     $(".menu-link").removeClass("is-active");
-     $(this).addClass("is-active");
-    });
-   });
-   
-   $(function () {
-    $(".main-header-link").click(function () {
-     $(".main-header-link").removeClass("is-active");
-     $(this).addClass("is-active");
-    });
-   });
-   
-   const dropdowns = document.querySelectorAll(".dropdown");
-   dropdowns.forEach((dropdown) => {
-    dropdown.addEventListener("click", (e) => {
-     e.stopPropagation();
-     dropdowns.forEach((c) => c.classList.remove("is-active"));
-     dropdown.classList.add("is-active");
-    });
-   });
-   
-   $(".search-bar input")
-    .focus(function () {
-     $(".header").addClass("wide");
-    })
-    .blur(function () {
-     $(".header").removeClass("wide");
-    });
-   
-   $(document).click(function (e) {
-    var container = $(".status-button");
-    var dd = $(".dropdown");
-    if (!container.is(e.target) && container.has(e.target).length === 0) {
-     dd.removeClass("is-active");
-    }
-   });
-   
-   $(function () {
-    $(".dropdown").on("click", function (e) {
-     $(".content-wrapper").addClass("overlay");
-     e.stopPropagation();
-    });
-    $(document).on("click", function (e) {
-     if ($(e.target).is(".dropdown") === false) {
-      $(".content-wrapper").removeClass("overlay");
-     }
-    });
-   });
-   
-   $(function () {
-    $(".status-button:not(.open)").on("click", function (e) {
-     $(".overlay-app").addClass("is-active");
-    });
-    $(".pop-up .close").click(function () {
-     $(".overlay-app").removeClass("is-active");
-    });
-   });
-   
-   $(".status-button:not(.open)").click(function () {
-    $(".pop-up").addClass("visible");
-   });
-   
-   $(".pop-up .close").click(function () {
-    $(".pop-up").removeClass("visible");
-   });
-   
-   const toggleButton = document.querySelector('.dark-light');
-   
-   toggleButton.addEventListener('click', () => {
-     document.body.classList.toggle('light-mode');
-   });
+$(function() {
+  $(".projects-wrapper").css("display", "none");
+});
+
+$(function() {
+  $(".menu-link").click(function() {
+      $(".menu-link").removeClass("is-active");
+      $(this).addClass("is-active");
+  });
+});
+
+$(function() {
+  $(".main-header-link").click(function() {
+      $(".main-header-link").removeClass("is-active");
+      $(this).addClass("is-active");
+  });
+});
+
+const dropdowns = document.querySelectorAll(".dropdown");
+dropdowns.forEach((dropdown) => {
+  dropdown.addEventListener("click", (e) => {
+      e.stopPropagation();
+      dropdowns.forEach((c) => c.classList.remove("is-active"));
+      dropdown.classList.add("is-active");
+  });
+});
+
+$(".search-bar input")
+  .focus(function() {
+      $(".header").addClass("wide");
+  })
+  .blur(function() {
+      $(".header").removeClass("wide");
+  });
+
+$(document).click(function(e) {
+  var container = $(".status-button");
+  var dd = $(".dropdown");
+  if (!container.is(e.target) && container.has(e.target).length === 0) {
+      dd.removeClass("is-active");
+  }
+});
+
+$(function() {
+  $(".dropdown").on("click", function(e) {
+      $(".content-wrapper").addClass("overlay");
+      e.stopPropagation();
+  });
+  $(document).on("click", function(e) {
+      if ($(e.target).is(".dropdown") === false) {
+          $(".content-wrapper").removeClass("overlay");
+      }
+  });
+});
+
+$(function() {
+  $(".status-button:not(.open)").on("click", function(e) {
+      $(".overlay-app").addClass("is-active");
+  });
+  $(".pop-up .close").click(function() {
+      $(".overlay-app").removeClass("is-active");
+  });
+});
+
+$(".status-button:not(.open)").click(function() {
+  $(".pop-up").addClass("visible");
+});
+
+$(".pop-up .close").click(function() {
+  $(".pop-up").removeClass("visible");
+});
+
+const toggleButton = document.querySelector('.dark-light');
+
+toggleButton.addEventListener('click', () => {
+  document.body.classList.toggle('light-mode');
+});
+
+const projectsTab = document.querySelector('.projects-wrapper')
+const homeTab = document.querySelector('.home-wrapper')
+
+function ShowHome() {
+  projectsTab.css("display", "none");
+  homeTab.css("display", "block");
+}
+
+function ShowProjects() {
+  homeTab.css("display", "none");
+  projectsTab.css("display", "block");
+}
